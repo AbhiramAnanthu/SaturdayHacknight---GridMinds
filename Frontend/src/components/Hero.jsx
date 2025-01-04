61ca6d7fb448c000365d69cd26b6830f622c7496
Mobing-ms <168826063+Mobing-ms@users.noreply.github.com>
2025-01-04 20:50:08 +0530

    Styling added to hero

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -12,16 +12,25 @@ const Hero = () => {
     )
       :
       (
-      <div className='text-center flex flex-col space-between'>
-        <h1>Enter the Dates to Check the asteroid sightings</h1>
-        <form>
-            <p><label>Enter the Start Date:</label>
-            <input type="date" name="date" id="date" /></p>
-            <p><label>Enter the End Date:</label>
-            <input type="date" name="date" id="date" /></p>
-            <button className='border px-4 py-1 mt-3 rounded-xl bg-blue-600 text-white'>Search</button>
-        </form>
-      </div>
+        <div className='flex justify-center items-center h-screen bg-[url("https://img.freepik.com/free-photo/galactic-night-sky-astronomy-science-combined-generative-ai_188544-9656.jpg?semt=ais_hybrid")] bg-cover bg-no-repeat bg-center'>
+  <div className="text-center p-6 bg-white bg-opacity-20 backdrop-blur-md rounded-xl">
+  <h1 className="text-xl font-bold mb-4 text-white text-3xl">Enter the Dates to Check the Asteroid Sightings</h1>
+  <form>
+    <p>
+      <label className="block mb-2 text-white">Enter the Start Date:</label>
+      <input type="date" name="start-date" id="start-date" className="border px-4 py-2 rounded-lg" />
+    </p>
+    <p>
+      <label className="block mb-2 text-white">Enter the End Date:</label>
+      <input type="date" name="end-date" id="end-date" className="border px-4 py-2 rounded-lg" />
+    </p>
+    <button className='border px-4 py-2 mt-4 rounded-xl bg-blue-600 text-white hover:bg-blue-700'>
+      Search
+    </button>
+  </form>
+</div>
+</div>
+
       )}
     </>
   )
