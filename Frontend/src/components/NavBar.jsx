14b6c984c16e5cab9b0e1c60855f2d54e4acbf6b
Mobing-ms <168826063+Mobing-ms@users.noreply.github.com>
2025-01-04 20:07:39 +0530

    nav bar completed

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const NavBar = () => {
   return (
     <>
-      <div className="text-black font-poppins">Hello World</div>
+      <div className=" flex justify-between text-white font-poppins bg-black h-[70px] pt-[12px] pb-[15px] font-bold text-4xl pl-[20px]">Hello World
+      <button className=" flex justify-end pt-2 text-white font-poppins  font-bold text-xl mr-[40px] px-4 py-2 rounded transition duration-300 hover:bg-green-700 hover:shadow-lg">Home</button>
+      </div>
     </>
   )
 }
