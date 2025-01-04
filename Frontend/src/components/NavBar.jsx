7de3277b3ca061c3451940a43468862b0a237b79
Blackeye6941 <leok37316@gmail.com>
2025-01-04 19:07:32 +0530

    tailwind debugged

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+      <div className="text-black">Hello World</div>
+    </>
   )
 }
 
