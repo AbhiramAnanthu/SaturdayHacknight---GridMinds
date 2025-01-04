07e4f1fc5654e779f4ab6d61cd09c0d40446424e
Mobing-ms <168826063+Mobing-ms@users.noreply.github.com>
2025-01-04 21:22:53 +0530

    tag name edited

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -4,8 +4,8 @@ const NavBar = () => {
     const navigate = useNavigate()
   return (
     <>
-      <div className=" flex justify-between text-white font-poppins bg-black h-[70px] pt-[12px] pb-[15px] font-bold text-4xl pl-[20px]">Hello World
-      <button className=" flex justify-end pt-2 text-white font-poppins  font-bold text-xl mr-[40px] px-4 py-2 rounded transition duration-300 hover:bg-orange-700 hover:shadow-lg" onClick={() => navigate("/")}>Home</button>
+      <div className=" flex justify-between text-white font-poppins bg-black h-[70px] pt-[15px] pb-[15px] font-bold text-4xl pl-[20px]">Astro Nav
+      <button className=" flex justify-end pt-1.5 text-white font-poppins  font-bold text-xl mr-[40px] px-4 py-2 rounded transition duration-300 hover:bg-orange-700 hover:shadow-lg" onClick={() => navigate("/")}>Home</button>
       </div>
     </>
   )
