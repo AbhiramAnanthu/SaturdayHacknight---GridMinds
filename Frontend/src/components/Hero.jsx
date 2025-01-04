a5b838762b02d7cd5d9fa8938aec0e065cb69712
Blackeye6941 <leok37316@gmail.com>
2025-01-04 19:33:15 +0530

    Changes to hero

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -14,10 +14,13 @@ const Hero = () => {
       (
       <div className='text-center flex flex-col space-between'>
         <h1>Enter the Dates to Check the asteroid sightings</h1>
-        <p><label>Enter the Start Date:</label>
-        <input type="date" name="date" id="date" /></p>
-        <p><label>Enter the End Date:</label>
-        <input type="date" name="date" id="date" /></p>
+        <form>
+            <p><label>Enter the Start Date:</label>
+            <input type="date" name="date" id="date" /></p>
+            <p><label>Enter the End Date:</label>
+            <input type="date" name="date" id="date" /></p>
+            <button className='border px-4 py-1 mt-3 rounded-xl bg-blue-600 text-white'>Search</button>
+        </form>
       </div>
       )}
     </>
