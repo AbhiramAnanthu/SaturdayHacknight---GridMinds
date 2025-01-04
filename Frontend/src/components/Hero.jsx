923960575acaeb01ba16da5135630130c3c842dc
Mobing-ms <168826063+Mobing-ms@users.noreply.github.com>
2025-01-04 21:08:41 +0530

    update navbar and hero

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -13,8 +13,8 @@ const Hero = () => {
       :
       (
         <div className='flex justify-center items-center h-screen bg-[url("https://img.freepik.com/free-photo/galactic-night-sky-astronomy-science-combined-generative-ai_188544-9656.jpg?semt=ais_hybrid")] bg-cover bg-no-repeat bg-center'>
-  <div className="text-center p-6 bg-white bg-opacity-20 backdrop-blur-md rounded-xl">
-  <h1 className="text-xl font-bold mb-4 text-white text-3xl">Enter the Dates to Check the Asteroid Sightings</h1>
+  <div className="text-center p-6 bg-white bg-opacity-20 backdrop-blur-sm rounded-xl">
+  <h1 className="text-xl font-bold mb-4 text-white text-2xl">Enter the Dates to Check the Asteroid Sightings</h1>
   <form>
     <p>
       <label className="block mb-2 text-white">Enter the Start Date:</label>
@@ -24,9 +24,9 @@ const Hero = () => {
       <label className="block mb-2 text-white">Enter the End Date:</label>
       <input type="date" name="end-date" id="end-date" className="border px-4 py-2 rounded-lg" />
     </p>
-    <button className='border px-4 py-2 mt-4 rounded-xl bg-blue-600 text-white hover:bg-blue-700'>
-      Search
-    </button>
+    <button className="border px-4 py-2 mt-4 rounded-xl bg-orange-600 text-white hover:bg-orange-700 hover:scale-105 hover:shadow-lg transition-all duration-300 ease-in-out">
+  Search
+</button>
   </form>
 </div>
 </div>
--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <>
       <div className=" flex justify-between text-white font-poppins bg-black h-[70px] pt-[12px] pb-[15px] font-bold text-4xl pl-[20px]">Hello World
-      <button className=" flex justify-end pt-2 text-white font-poppins  font-bold text-xl mr-[40px] px-4 py-2 rounded transition duration-300 hover:bg-green-700 hover:shadow-lg" onClick={() => navigate("/")}>Home</button>
+      <button className=" flex justify-end pt-2 text-white font-poppins  font-bold text-xl mr-[40px] px-4 py-2 rounded transition duration-300 hover:bg-orange-700 hover:shadow-lg" onClick={() => navigate("/")}>Home</button>
       </div>
     </>
   )
