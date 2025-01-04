23ee9960b0b68661bbf170be02b758d2fbdff9d3
Blackeye6941 <leok37316@gmail.com>
2025-01-04 21:14:01 +0530

    Slight change for design

--- Frontend/src/components/Asteroid.jsx
+++ Frontend/src/components/Asteroid.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 const Asteroid = () => {
   return (
     <>
-      <p className='flex flex-col text-center mt-10'>
+      <p className='text-center mt-10'>
         <h1>
             Asteroid ID
         </h1>
         <h1>Closest approach Date</h1>
         <h1>Speed</h1>
-        <h1>Click the button below to receive notifcation</h1>
-        <button></button>
+        <h1>If you want to receive notification on the asteroid sighting</h1>
+        <button className='text-white border-2 bg-orange-500 px-2 py-1 rounded-xl'>Click Here</button>
       </p>
     </>
   )
